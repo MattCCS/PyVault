4d02e2431bd3068fda22a6a2f51d5e7af9e2b44e
Matthew Cotton <matt@thecottons.com>
2016-06-09 15:21:40 -0400

    began creating API

---
+++ pykeys/api.py
@@ -0,0 +1,13 @@
+
+def load(): raise NotImplementedError()
+def save(): raise NotImplementedError()
+
+def decrypt(): raise NotImplementedError()
+def encrypt(): raise NotImplementedError()
+
+def check_master_password(): raise NotImplementedError()
+
+def show_entry(): raise NotImplementedError()
+def add_entry(): raise NotImplementedError()
+def edit_entry(): raise NotImplementedError()
+def delete_entry(): raise NotImplementedError()
---
+++ pykeys/pwm.py
@@ -0,0 +1,7 @@
+"""
+"""
+
+class PasswordManager(object):
+
+    def __init__(self):
+        pass
