c0e87a1bf731e0c7c800a599b0b33885e484b92b
Matthew Cotton <matt@thecottons.com>
2016-07-18 22:18:29 -0400

    added boilerplate meta files (auto-generated)

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""
+Auto-generated setup.py file.
+"""
+
+import setuptools
+
+setuptools.setup(
+    name='pyvault',
+    version=open('VERSION.txt').read(),
+    author='Matthew Cotton',
+    author_email='matt@thecottons.com',
+    packages=['pyvault'],
+    # py_modules=[],
+    # scripts=[],
+    url='http://pypi.python.org/pypi/pyvault/',
+    license='MIT - LICENSE.txt',
+    description='Simple, secure, API-driven password manager',
+    long_description=open('README.rst').read(),
+    install_requires=[],
+)
